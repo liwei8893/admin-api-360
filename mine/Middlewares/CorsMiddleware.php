2e296a763b7ee6f0e62dd6e61c7ad0ce3af112e9
liwei <liwei8893@outlook.com>
2022-08-23 13:49:03 +0800

    feat: 添加跨域中间件

--- mine/Middlewares/CorsMiddleware.php
+++ mine/Middlewares/CorsMiddleware.php
@@ -15,8 +15,10 @@ class CorsMiddleware implements MiddlewareInterface
         $response = Context::get(ResponseInterface::class);
         $response = $response->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
+            ->withHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE')
+
             // Headers 可以根据实际情况进行改写。
-            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,sec-ch-ua-mobile,sec-ch-ua-platform,sec-ch-ua,Referer,Accept-Language');
+            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,sec-ch-ua-mobile,sec-ch-ua-platform,sec-ch-ua,Referer,Accept-Language,Accept');
 
         Context::set(ResponseInterface::class, $response);
 
