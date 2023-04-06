f8ae80043367a4ae80c0f08c73feadd332757a7b
liwei <liwei8893@outlook.com>
2023-04-06 17:26:48 +0800

    fix: 修复课程购买记录平台筛选

--- app/Course/Mapper/CourseHistoryMapper.php
+++ app/Course/Mapper/CourseHistoryMapper.php
@@ -26,13 +26,22 @@ class CourseHistoryMapper extends AbstractMapper
             ->with(['users:id,user_name,old_platform,platform,mobile,remark', 'orderGrade', 'orderSubject'])
             ->whereHas('users', function (Builder $query) use ($data) {
                 $query->where('user_type', User::USER_TYPE)
-                    ->when(! empty($data['users_mobile'] && is_array($data['users_mobile'])), function (Builder $query) use ($data) {
+                    ->when(! empty($data['users_mobile']) && is_array($data['users_mobile']), function (Builder $query) use ($data) {
                         $query->whereIn('mobile', $data['users_mobile']);
                     })
-                    ->when(! empty($data['users_mobile'] && ! is_array($data['users_mobile'])), function (Builder $query) use ($data) {
+                    ->when(! empty($data['users_mobile']) && ! is_array($data['users_mobile']), function (Builder $query) use ($data) {
                         $query->where('mobile', $data['users_mobile']);
                     })
+                    ->when(! empty($data['users_platform']), function (Builder $query) use ($data) {
+                        $query->where('platform', $data['users_platform']);
+                    })
                     ->platformDataScope();
+            })
+            ->when(isset($data['created_at'][0], $data['created_at'][1]), function (Builder $query) use ($data) {
+                $query->whereBetween(
+                    'created_at',
+                    [strtotime($data['created_at'][0]), strtotime($data['created_at'][1]) + 86400]
+                );
             })
             ->where('shop_id', $data['shop_id'])
             ->orderBy('created_at', 'desc')
