c731dd612c478a4fa1a7fd9de031b2039844c3a9
liwei <liwei8893@outlook.com>
2023-05-20 15:53:28 +0800

    fix: 修复未注册登录报错信息

--- app/Users/Service/UsersAppLoginService.php
+++ app/Users/Service/UsersAppLoginService.php
@@ -68,6 +68,9 @@ class UsersAppLoginService extends AbstractService
                 // 验证成功
                 return $this->loginAfter($userinfo);
             }
+            if (! $userinfo) {
+                throw new NormalStatusException('该账号未注册,请联系课程顾问!');
+            }
             // 密码验证
             if ($this->mapper->checkPass($params['user_pass'], $userinfo['user_pass'])) {
                 // 验证成功
@@ -81,6 +84,72 @@ class UsersAppLoginService extends AbstractService
             }
             throw new NormalStatusException($e->getMessage());
         }
+    }
+
+    /**
+     * 前台注册.
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function register(array $data): Model|MineModel
+    {
+        if ($this->usersService->existsByMobile($data['mobile'])) {
+            throw new NormalStatusException('手机号已存在');
+        }
+        // 获取平台编号,挂载到数组
+        $data = $this->userSalePlatformService->withPlatformNum($data);
+        // 合并初始化参数
+        $data = array_merge([
+            'mobile' => $data['mobile'],
+            'user_name' => $data['user_name'] ?? $this->usersService->getInitUserName($data['mobile']),
+            'user_nickname' => $data['user_name'] ?? $this->usersService->getInitUserName($data['mobile']),
+            'real_name' => $data['real_name'] ?? $this->usersService->getInitUserName($data['mobile']),
+            'user_pass' => $data['user_pass'] ?? $this->usersService->getInitPassword($data['mobile']),
+            'avatar' => config('hxt-app.defaultAvatar'),
+            'user_type' => 1,
+            'status' => 1,
+            'sex' => 3,
+            'last_login_ip' => container()->get(MineRequest::class)->ip(),
+            'last_login_time' => time(),
+        ], $data);
+        return $this->mapper->create($data);
+    }
+
+    /**
+     * @param mixed $userModel
+     * @return mixed
+     * @throws ContainerExceptionInterface
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     */
+    public function loginAfter(User $userModel): array
+    {
+        $request = container()->get(MineRequest::class);
+        // 生成jwt token
+        $token = user('app')->getToken(['id' => $userModel->id]);
+        // 更新最后登录时间
+        $userModel->update([
+            'last_login_ip' => $request->ip(),
+            'last_login_time' => time(),
+            'remember_token' => $token,
+        ]);
+        // 插入登录日志表
+        $this->mapper->setLoginLog(['users_id' => $userModel->id]);
+        // 已购买的课程id列表挂载到用户信息中
+        $userModel->load(['orders' => static function ($query) {
+            $query->normalOrder()->isNotExpire()->select(['user_id', 'shop_id']);
+        }]);
+        $orderIds = $userModel->orders->pluck('shop_id');
+        // 挂载会员类型,到期时间
+        $userModel->load(['vipType']);
+        // 复制用户模型
+        $result = $userModel->toArray();
+        $result['orders'] = $orderIds->toArray();
+        // 添加是否初始密码
+        $result['isSimplePwd'] = $this->mapper->hasSimplePwd($userModel);
+        // 添加token
+        $result['remember_token'] = $token;
+        return $result;
     }
 
     /**
@@ -139,72 +208,6 @@ class UsersAppLoginService extends AbstractService
         } catch (Exception $e) {
             throw new NormalStatusException($e->getMessage());
         }
-    }
-
-    /**
-     * @param mixed $userModel
-     * @return mixed
-     * @throws ContainerExceptionInterface
-     * @throws InvalidArgumentException
-     * @throws NotFoundExceptionInterface
-     */
-    public function loginAfter(User $userModel): array
-    {
-        $request = container()->get(MineRequest::class);
-        // 生成jwt token
-        $token = user('app')->getToken(['id' => $userModel->id]);
-        // 更新最后登录时间
-        $userModel->update([
-            'last_login_ip' => $request->ip(),
-            'last_login_time' => time(),
-            'remember_token' => $token,
-        ]);
-        // 插入登录日志表
-        $this->mapper->setLoginLog(['users_id' => $userModel->id]);
-        // 已购买的课程id列表挂载到用户信息中
-        $userModel->load(['orders' => static function ($query) {
-            $query->normalOrder()->isNotExpire()->select(['user_id', 'shop_id']);
-        }]);
-        $orderIds = $userModel->orders->pluck('shop_id');
-        // 挂载会员类型,到期时间
-        $userModel->load(['vipType']);
-        // 复制用户模型
-        $result = $userModel->toArray();
-        $result['orders'] = $orderIds->toArray();
-        // 添加是否初始密码
-        $result['isSimplePwd'] = $this->mapper->hasSimplePwd($userModel);
-        // 添加token
-        $result['remember_token'] = $token;
-        return $result;
-    }
-
-    /**
-     * 前台注册.
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
-    public function register(array $data): Model|MineModel
-    {
-        if ($this->usersService->existsByMobile($data['mobile'])) {
-            throw new NormalStatusException('手机号已存在');
-        }
-        // 获取平台编号,挂载到数组
-        $data = $this->userSalePlatformService->withPlatformNum($data);
-        // 合并初始化参数
-        $data = array_merge([
-            'mobile' => $data['mobile'],
-            'user_name' => $data['user_name'] ?? $this->usersService->getInitUserName($data['mobile']),
-            'user_nickname' => $data['user_name'] ?? $this->usersService->getInitUserName($data['mobile']),
-            'real_name' => $data['real_name'] ?? $this->usersService->getInitUserName($data['mobile']),
-            'user_pass' => $data['user_pass'] ?? $this->usersService->getInitPassword($data['mobile']),
-            'avatar' => config('hxt-app.defaultAvatar'),
-            'user_type' => 1,
-            'status' => 1,
-            'sex' => 3,
-            'last_login_ip' => container()->get(MineRequest::class)->ip(),
-            'last_login_time' => time(),
-        ], $data);
-        return $this->mapper->create($data);
     }
 
     /**
