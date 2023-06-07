d610f4f89945fbf8be9e35f4a7864271113bd9fd
liwei <liwei8893@outlook.com>
2023-06-07 10:35:44 +0800

    feat: 测试线上微信消息

--- app/Operation/Queue/Producer/SendWxMsgProducer.php
+++ app/Operation/Queue/Producer/SendWxMsgProducer.php
@@ -6,16 +6,12 @@ namespace App\Operation\Queue\Producer;
 
 use Hyperf\Amqp\Annotation\Producer;
 use Hyperf\Amqp\Message\ProducerMessage;
-use JsonException;
 
-#[Producer(exchange: 'mineadmin', routingKey: 'wx_msg.routing')]
+#[Producer(exchange: 'mineadmin', routingKey: 'wxMsg.routing')]
 class SendWxMsgProducer extends ProducerMessage
 {
-    /**
-     * @throws JsonException
-     */
     public function __construct(mixed $data)
     {
-        $this->payload = (is_array($data) || is_object($data)) ? json_encode($data, JSON_THROW_ON_ERROR) : $data;
+        $this->payload = (is_array($data) || is_object($data)) ? json_encode($data) : $data;
     }
 }
